5117961ded811d39d83415839c81ffea6d861083
agojkovic <gusgojal@student.gu.se>
2020-12-10 00:22:50 +0100

    Ålderskontroll preliminärt klar

--- AlkoApp/lib/main.dart
+++ AlkoApp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:AlkoApp/views/AgeView.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,8 +15,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomeListview(),
+    return MaterialApp(     
+      debugShowCheckedModeBanner: false,
+      home: AgeView(),
     );
   }
 }
