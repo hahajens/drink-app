9632d44a138d08cd29983a503eda7e59685e80d5
TomatensHerre <pannebo2@gmail.com>
2020-12-14 18:07:39 +0100

    Statisks filter för homelistview klar

--- AlkoApp/lib/model/Model.dart
+++ AlkoApp/lib/model/Model.dart
@@ -4,9 +4,9 @@ import 'package:AlkoApp/DB/DB.dart';
 
 class Model extends ChangeNotifier {
   List<AlkoObject> alkoList = new List();
-  List<AlkoObject> filteredList = new List();
+  List<String> listToFilterOn = new List();
 
-  bool returnFiltered = false;
+  bool filterList = false;
   int index;
 
   Model() {
@@ -18,19 +18,5 @@ class Model extends ChangeNotifier {
     alkoList = await DB.getData();
     notifyListeners();
     print("DONE!");
-  }
-
-//TODO
-  List<AlkoObject> getList({input}) {
-    if (returnFiltered == true) {
-      returnFiltered = false;
-      //where-funktion
-      filteredList =
-          alkoList.where((element) => element.getStrDrink == input).toList();
-      return filteredList;
-    } else {
-      returnFiltered = true;
-      return alkoList;
-    }
   }
 }
--- AlkoApp/lib/views/MyHomeListview.dart
+++ AlkoApp/lib/views/MyHomeListview.dart
@@ -1,27 +1,36 @@
-import 'package:AlkoApp/views/ObjectInfoView.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:AlkoApp/model/Model.dart';
 
+//TODO ändra hur height och width sätts
 class MyHomeListview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: _myHomeAppbar(),
-      drawer: _myHomeDrawer(),
-      body: Column(
-        children: [
-          _mySearchBar(),
-          Expanded(child: MyCustomListview()),
-        ],
+    return Consumer<Model>(
+      builder: (context, state, child) => Scaffold(
+        appBar: _myHomeAppbar(),
+        drawer: _myHomeDrawer(),
+        body: Column(
+          children: [
+            //_mySearchBar(),
+            Container(
+              height: 50,
+              width: 10000,
+              child: Padding(
+                  padding: EdgeInsets.fromLTRB(15, 5, 15, 5),
+                  child: _filterButton(context, state)),
+            ),
+            Expanded(child: _myCustomListview(state.alkoList)),
+          ],
+        ),
       ),
     );
   }
 
   Widget _myHomeAppbar() {
     return AppBar(
-      title: Text("NIKLAS"),
+      title: Text("Söksida/filtrering"),
     );
   }
 
@@ -42,33 +51,100 @@ class MyHomeListview extends StatelessWidget {
     );
   }
 
-  Widget _mySearchBar() {
-    TextEditingController controller = new TextEditingController();
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(
-        border: InputBorder.none,
-        hintText: 'Filter',
-        hintStyle: TextStyle(color: Colors.grey),
-      ),
+  //TODO '
+  //1. fyll en lista med alla spritsorter i model och fyll filtret
+  //3. Hur gör man query för "hela" db? alt hur gör man en query för bara spritstorterna?
+  //4. gör färdigt _filterList()
+
+  //Ska returnera en lista som filtrerats på listToFilterOn
+  _filterList(state, alkoList) {
+    List returnList;
+    if (state.listToFilterOn.length > 0) {
+      return returnList = alkoList
+          .where((i) => i.strIngredient1 == state.listToFilterOn)
+          .toList();
+    } else {
+      return alkoList;
+    }
+  }
+
+  Widget _filterButton(BuildContext context, state) {
+    return FlatButton(
+      color: Colors.blueAccent,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
+      onPressed: () {
+        _showAlertDialog(context, state);
+      },
+      child: Text("Filtrera"),
     );
   }
-}
 
-class MyCustomListview extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+  _showAlertDialog(BuildContext context, state) {
+    List listOfLiqour = [
+      "vodka"
+    ]; //<-- ska komma ifrån model eller DB kanske? //TODO ska den vara statisk?
+    //Lista med strängar att sortera på
+    List listToFilterOn = [];
+
+    Widget okButton = FlatButton(
+      child: Text("Filtrera"),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+
+    Widget cancelButton = FlatButton(
+      child: Text("Avbryt"),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Filtermeny"),
+          content: Container(
+            width: MediaQuery.of(context).size.width * .7,
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: List.generate(listOfLiqour.length, (index) {
+                return Container(
+                  child: GestureDetector(
+                    onTap: () {
+                      print("Tapped: ${listOfLiqour[index]}");
+                      listToFilterOn.add(listOfLiqour[index]);
+                    },
+                    child: Card(
+                      color: Colors.blue,
+                      child: Text("${listOfLiqour[index]}"),
+                    ),
+                  ),
+                );
+              }),
+            ),
+          ),
+          actions: [
+            cancelButton,
+            okButton,
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _myCustomListview(list) {
     return Consumer<Model>(
       builder: (context, state, child) => ListView.builder(
-        itemCount: state.alkoList.length,
+        itemCount: list.length,
         itemBuilder: (context, index) {
           return ListTile(
               leading: CircleAvatar(
-                backgroundImage:
-                    NetworkImage(state.alkoList[index].strDrinkThumb),
+                backgroundImage: NetworkImage(list[index].strDrinkThumb),
               ),
-              title: Text("${state.alkoList[index].strDrink}"),
-              trailing: Text("${state.alkoList[index].strAlcoholic}"),
+              title: Text("${list[index].strDrink}"),
+              trailing: Text("${list[index].strAlcoholic}"),
               onTap: () {
                 state.index = index;
                 Navigator.pushNamed(context, '/ObjectInfoview');
@@ -78,3 +154,22 @@ class MyCustomListview extends StatelessWidget {
     );
   }
 }
+
+// Widget _mySearchBar() {
+//   TextEditingController controller = new TextEditingController();
+//   return Row(
+//     children: [
+//       Expanded(
+//         child: TextField(
+//           controller: controller,
+//           decoration: InputDecoration(
+//             contentPadding: EdgeInsets.all(8.0),
+//             border: InputBorder.none,
+//             hintText: 'Sök på Drinknamn:',
+//             hintStyle: TextStyle(color: Colors.grey),
+//           ),
+//         ),
+//       ),
+//     ],
+//   );
+// }
