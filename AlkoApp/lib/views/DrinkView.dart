3fb2a72a9b2252731bbf7989d357a76a02168449
hahajens <guskarjeck@student.gu.se>
2020-12-15 08:41:27 +0100

    All elements connected to api except ingredients.

--- AlkoApp/lib/views/DrinkView.dart
+++ AlkoApp/lib/views/DrinkView.dart
@@ -5,67 +5,65 @@ import 'package:flutter/rendering.dart';
 class DrinkView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final AlkoObject obj = ModalRoute.of(context).settings.arguments;
+    final AlkoObject drink = ModalRoute.of(context).settings.arguments;
     return Scaffold(
-      backgroundColor: Colors.white60,
+      backgroundColor: Colors.white70,
       body: ListView(
         children: [
           Container(
-            //padding: EdgeInsets.symmetric(vertical: 15),
             decoration: BoxDecoration(
-                color: Colors.white70,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(25),
-                  bottomRight: Radius.circular(25),
-                )),
+                color: Colors.white70, borderRadius: BorderRadius.circular(25)),
             child: Column(
               children: [
-                Container(
-                  child: Image(
-                    image: NetworkImage(
-                        //bild finns i strDrinkThumb
-                        "https://images.unsplash.com/photo-1470337458703-46ad1756a187?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80"),
-                  ),
+                Image(
+                  image: NetworkImage(drink.strDrinkThumb
+                      //bild finns i strDrinkThumb
+                      ),
                 ),
                 Row(
                   children: [
                     _tagWidget(//strTags
-                        "Strong Flavour"),
-                    _tagWidget("Morning Drink"),
-                    _tagWidget("Hangover"),
+                        drink.strTags),
                   ],
                 ),
               ],
             ),
           ),
-          Row(
-            children: [
-              _titleWidget(//strDrink
-                  obj.strDrink),
-              IconButton(
-                icon: Icon(Icons.favorite_border_outlined),
-                iconSize: 34,
-                onPressed: () {
-                  // Lägg till i favoritlista
-                },
-              )
-            ],
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    _titleWidget(//strDrink
+                        drink.strDrink),
+                    IconButton(
+                      icon: Icon(Icons.favorite_border_outlined),
+                      iconSize: 34,
+                      onPressed: () {
+                        // Lägg till i favoritlista
+                      },
+                    )
+                  ],
+                ),
+                _categoryWidget(//strCategory
+                    drink.strCategory),
+                _alcoholWidget(//strAlcoholic
+                    drink.strAlcoholic),
+                _glassWidget(//strGlass
+                    drink.strGlass),
+                _customDivider(),
+                _ingredientsWidget(
+                    ingredient1: "4cl of Tequila",
+                    ingredient2: "2cl of Triple sec",
+                    ingredient3: "4cl of Lime juice",
+                    ingredient4: "Salt"),
+                _customDivider(),
+                _instructionWidget(//strInstructions
+                    drink.strInstructions),
+              ],
+            ),
           ),
-          _categoryWidget(//strCategory
-              "Cocktail"),
-          _alcoholWidget(//strAlcoholic
-              "Alcoholic"),
-          _glassWidget(//strGlass
-              "Cocktail Glass"),
-          _customDivider(),
-          _ingredientsWidget(
-              ingredient1: "4cl of Tequila",
-              ingredient2: "2cl of Triple sec",
-              ingredient3: "4cl of Lime juice",
-              ingredient4: "Salt"),
-          _customDivider(),
-          _instructionWidget(//strInstructions
-              "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass."),
         ],
       ),
     );
@@ -83,7 +81,6 @@ Widget _ingredientsWidget({
     children: [
       Container(
         alignment: Alignment.topLeft,
-        padding: EdgeInsets.only(left: 15),
         child: Text(
           "Ingredients",
           style: TextStyle(
@@ -94,7 +91,7 @@ Widget _ingredientsWidget({
       ),
       Container(
         alignment: Alignment.topLeft,
-        padding: EdgeInsets.only(left: 15, top: 10),
+        padding: EdgeInsets.only(top: 10),
         child: Text(
           ingredient1,
           style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
@@ -102,7 +99,7 @@ Widget _ingredientsWidget({
       ),
       Container(
         alignment: Alignment.topLeft,
-        padding: EdgeInsets.only(left: 15, top: 5),
+        padding: EdgeInsets.only(top: 5),
         child: Text(
           ingredient2,
           style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
@@ -110,7 +107,7 @@ Widget _ingredientsWidget({
       ),
       Container(
         alignment: Alignment.topLeft,
-        padding: EdgeInsets.only(left: 15, top: 5),
+        padding: EdgeInsets.only(top: 5),
         child: Text(
           ingredient3,
           style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
@@ -118,7 +115,7 @@ Widget _ingredientsWidget({
       ),
       Container(
         alignment: Alignment.topLeft,
-        padding: EdgeInsets.only(left: 15, top: 5),
+        padding: EdgeInsets.only(top: 5),
         child: Text(
           ingredient4,
           style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
@@ -143,7 +140,7 @@ Widget _glassWidget(String glass) {
     children: [
       Container(
         alignment: Alignment.topLeft,
-        padding: EdgeInsets.only(left: 15, top: 15),
+        padding: EdgeInsets.only(top: 15),
         child: Text(
           "Serve in",
           style: TextStyle(
@@ -155,7 +152,6 @@ Widget _glassWidget(String glass) {
       ),
       Container(
           alignment: Alignment.topLeft,
-          padding: EdgeInsets.only(left: 15),
           child: Text(
             glass,
             style: TextStyle(
@@ -171,7 +167,7 @@ Widget _glassWidget(String glass) {
 Widget _alcoholWidget(String alcohol) {
   return Container(
     alignment: Alignment.topLeft,
-    padding: EdgeInsets.only(left: 15, top: 15),
+    padding: EdgeInsets.only(top: 15),
     child: Text(
       alcohol,
       style: TextStyle(
@@ -186,7 +182,7 @@ Widget _alcoholWidget(String alcohol) {
 Widget _categoryWidget(String category) {
   return Container(
     alignment: Alignment.topLeft,
-    padding: EdgeInsets.only(left: 15, top: 15),
+    padding: EdgeInsets.only(top: 15),
     child: Text(
       category,
       style: TextStyle(
@@ -203,7 +199,6 @@ Widget _instructionWidget(String instruction) {
     children: [
       Container(
         alignment: Alignment.topLeft,
-        padding: EdgeInsets.only(left: 15),
         child: Text(
           "Instructions",
           style: TextStyle(
@@ -213,7 +208,7 @@ Widget _instructionWidget(String instruction) {
         ),
       ),
       Container(
-        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        padding: EdgeInsets.symmetric(vertical: 10),
         child: Text(
           instruction,
           style: TextStyle(
@@ -227,36 +222,41 @@ Widget _instructionWidget(String instruction) {
 }
 
 Widget _titleWidget(String title) {
-  return Container(
-    padding: EdgeInsets.only(left: 15),
-    alignment: Alignment.topLeft,
-    child: Text(
-      title,
-      textAlign: TextAlign.left,
-      style: TextStyle(
-        fontSize: 42,
-        fontWeight: FontWeight.bold,
+  return Expanded(
+    child: Container(
+      alignment: Alignment.topLeft,
+      child: Text(
+        title,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontSize: 42,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     ),
   );
 }
 
 Widget _tagWidget(String tag) {
-  return Container(
-    decoration: BoxDecoration(
-        border: Border.all(
-          width: 2,
-          color: Colors.grey,
-        ),
-        borderRadius: BorderRadius.circular(15)),
-    margin: EdgeInsets.only(left: 15, bottom: 20, top: 20),
-    padding: EdgeInsets.all(4),
-    // TAGGAR, finns i strTags.
-    child: Text(tag,
-        style: TextStyle(
-          fontSize: 14,
-          fontWeight: FontWeight.w600,
-          color: Colors.grey[600],
-        )),
-  );
+  if (tag == null) {
+    return Text("", style: TextStyle(fontSize: 32));
+  } else {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(
+            width: 2,
+            color: Colors.grey,
+          ),
+          borderRadius: BorderRadius.circular(15)),
+      margin: EdgeInsets.only(left: 15, bottom: 20, top: 20),
+      padding: EdgeInsets.all(4),
+      // TAGGAR, finns i strTags.
+      child: Text(tag,
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+            color: Colors.grey[600],
+          )),
+    );
+  }
 }
