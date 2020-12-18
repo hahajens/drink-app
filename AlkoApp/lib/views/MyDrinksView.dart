666a9bb81cc8ce8f1cbee14711057e7a793fb8ec
Niklas L <guslandsni@student.gu.se>
2020-12-18 16:45:56 +0100

    Lade till nytt utseende på populära drinkar

--- AlkoApp/lib/views/MyDrinksView.dart
+++ AlkoApp/lib/views/MyDrinksView.dart
@@ -1,68 +1,87 @@
+import 'package:AlkoApp/widgets/PopularDrinkCarousel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../model/Model.dart';
 import 'package:AlkoApp/model/NavigationBar.dart';
+
 class MyDrinksView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'My recepies',
-          style: TextStyle(fontFamily: "Tomorrow"),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFFF4F4F4),
+        body: ListView(
+          children: [
+            PopularDrinkCarousel(),
+            // PopularIngredientsCarousel(),
+            // RandomDrinksCarousel()
+          ],
         ),
       ),
-      body: MyDrinksList(),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
-        label: Text('Add new Drink'),
-        icon: Icon(Icons.add),
-      ),
-      /*bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0,
-          type: BottomNavigationBarType.fixed,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text('Home'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              title: Text('Search'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.favorite),
-              title: Text('Favorites'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.local_drink),
-              title: Text('My Drinks'),
-            ),
-          ]),*/
-          bottomNavigationBar: CustomNavigationBar()
     );
   }
 }
+// class MyDrinksView extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           'My recepies',
+//           style: TextStyle(fontFamily: "Tomorrow"),
+//         ),
+//       ),
+//       body: MyDrinksList(),
+//       floatingActionButton: FloatingActionButton.extended(
+//         onPressed: () {},
+//         label: Text('Add new Drink'),
+//         icon: Icon(Icons.add),
+//       ),
+//       /*bottomNavigationBar: BottomNavigationBar(
+//           currentIndex: 0,
+//           type: BottomNavigationBarType.fixed,
+//           items: [
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.home),
+//               title: Text('Home'),
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.search),
+//               title: Text('Search'),
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.favorite),
+//               title: Text('Favorites'),
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.local_drink),
+//               title: Text('My Drinks'),
+//             ),
+//           ]),*/
+//           bottomNavigationBar: CustomNavigationBar()
+//     );
+//   }
+// }
 
-class MyDrinksList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<Model>(
-      builder: (context, state, child) => ListView.builder(
-        itemCount: state.alkoList.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-              leading: CircleAvatar(
-                backgroundImage:
-                    NetworkImage(state.alkoList[index].strDrinkThumb),
-              ),
-              title: Text("${state.alkoList[index].strDrink}"),
-              trailing: Text("${state.alkoList[index].strAlcoholic}"),
-              onTap: () {});
-        },
-      ),
-    );
-  }
-}
+// class MyDrinksList extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Consumer<Model>(
+//       builder: (context, state, child) => ListView.builder(
+//         itemCount: state.alkoList.length,
+//         itemBuilder: (context, index) {
+//           return ListTile(
+//               leading: CircleAvatar(
+//                 backgroundImage:
+//                     NetworkImage(state.alkoList[index].strDrinkThumb),
+//               ),
+//               title: Text("${state.alkoList[index].strDrink}"),
+//               trailing: Text("${state.alkoList[index].strAlcoholic}"),
+//               onTap: () {});
+//         },
+//       ),
+//     );
+//   }
+// }
--- AlkoApp/lib/views/StartView.dart
+++ AlkoApp/lib/views/StartView.dart
@@ -5,11 +5,28 @@ class StartView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(),
+      body: ListView(children: [
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 100),
+          height: 50.0,
+          child: RaisedButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+                side: BorderSide.none),
+            onPressed: () {},
+            padding: EdgeInsets.all(10.0),
+            color: Color.fromRGBO(0, 160, 227, 1),
+            textColor: Colors.white,
+            child: Text("Surprise me!", style: TextStyle(fontSize: 25)),
+          ),
+        ),
+        Container(
+            child: Column(
+          children: [],
+        ))
+      ]),
       bottomNavigationBar: CustomNavigationBar(),
-
-      body: Text('Hej'),
-
     );
   }
 }
