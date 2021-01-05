fa8be85648f995d00308b635b40d178255660946
agojkovic <gusgojal@student.gu.se>
2021-01-05 14:32:59 +0100

    Fixat i favoriteView och DrinkView

--- AlkoApp/lib/views/MyFavoritesView.dart
+++ AlkoApp/lib/views/MyFavoritesView.dart
@@ -8,65 +8,96 @@ class MyFavoritesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<Model>(
       builder: (context, state, child) => Scaffold(
-        body: _myFavorites(state.favoriteList),
+        body: _myFavorites(state, context, state.favoriteList),
         bottomNavigationBar: CustomNavigationBar(),
       ),
     );
   }
 
-  Widget _myFavorites(favoriteList) {
-    return Consumer<Model>(
-        builder: (context, state, child) => Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: GridView.count(
-                crossAxisCount: 2,
-                children: List.generate(
-                    favoriteList.length,
-                    (index) => Stack(children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20.0),
-                          child: Image(
+  Widget _myFavorites(state, context, favoriteList) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(
+            favoriteList.length,
+            (index) => Stack(children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                Navigator.pushNamed(context, '/DrinkView',
+                        arguments: favoriteList[index].idDrink);                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image(
                           image: NetworkImage(
                               Provider.of<Model>(context, listen: false)
                                   .favoriteList[index]
                                   .strDrinkThumb),
                         ),
-                        ),
                       ),
-                      Positioned(
-                        top: 10,
-                        right: 10,
-                          child: IconButton(
-                          icon: Icon(Icons.delete, color: Colors.white, size: 30.0),
-                          onPressed: () {
-                            state.removeFavorite(state.favoriteList[index]);
-                          },
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Text(
-                        "${favoriteList[index].strDrink}",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 35.0,
-                          fontWeight: FontWeight.w600,
-                          letterSpacing: 1.2,
-                          shadows: <Shadow>[
-                         Shadow(
-                           offset: Offset(4.0, 3.0),
-                           blurRadius: 15.0,
-                           color: Color.fromARGB(255, 0, 0, 0),
-                         ),
-                          ],
-                        ),
-                      ),
-                      )
-
-                    ])),
+                    ),
+                  ),
+                  Positioned(
+                    top: 10,
+                    right: 10,
+                    child: Container(
+                    decoration: BoxDecoration(
+                    boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0.0, 2.0),
+                      blurRadius: 40.0,
+                ),
+               ],
               ),
-            ));
+                      child: IconButton(
+                        icon: Icon(Icons.delete, color: Colors.white, size: 30.0),
+                        onPressed: () {
+                          state.removeFavorite(state.favoriteList[index]);
+                        },
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Text(
+                      "${favoriteList[index].strDrink}",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1.2,
+                        shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(4.0, 3.0),
+                            blurRadius: 15.0,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ])),
+      ),
+    );
   }
 }
+
+// class MyFavoritesView extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Expanded(
+//       child: Consumer<Model>(
+//           builder: (context, state, child) => (state.isLoading == true)
+//               ? Spinner()
+//               : GridView.count(
+//                   crossAxisCount: 2,
+//                   children: List.generate(state.favoriteList.length, (index) {
+//                     return CreateDrinkContainer(state.favoriteList[index]);
+//                   }),
+//                 )),
+//     );
+//   }
+// }
