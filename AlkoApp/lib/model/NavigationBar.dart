c9b725cbaba945b5effc2152cd72a4bf71bf3f90
Martin <guseklmat@student.gu.se>
2021-01-05 18:59:28 +0100

    Lagt till att navbar ändrar färg

--- AlkoApp/lib/model/NavigationBar.dart
+++ AlkoApp/lib/model/NavigationBar.dart
@@ -1,17 +1,16 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'MediaSize.dart';
+import 'Model.dart';
 
 class CustomNavigationBar extends StatelessWidget {
-
-  final Color _mainColor =
-
-  Colors.white; 
-  final Color _iconColor = Colors.blueGrey[400]; 
+  final Color _mainColor = Colors.white;
   final double _iconSize = 0.05; //
   final double _fontSize = 0.025;
 
   Widget build(BuildContext context) {
+    var provider = Provider.of<Model>(context);
     return Container(
       height: Platform.isAndroid
           ? displayHeight(context) * 0.1
@@ -25,54 +24,70 @@ class CustomNavigationBar extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Column(
-              children: [
-                IconButton(
-                    icon: Icon(Icons.lightbulb_outline, //home_outlined,
-                        size: displayHeight(context) * _iconSize,
-                        color: _iconColor),
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(context, '/StartView');
-                    }),
-
-                Text("Inspiration",
-                    style: TextStyle(
-                        fontSize: displayHeight(context) * _fontSize)) //_fontSize
-              ],
+            Container(
+              width: displayWidth(context) * 0.3,
+              child: Column(
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.lightbulb_outline, //home_outlined,
+                          size: displayHeight(context) * _iconSize,
+                          color: provider.getIconColor(0)), //_iconColor),
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/StartView');
+                        provider.setIconColor(0);
+                      }),
+                  Text("Inspiration",
+                      style: TextStyle(
+                          fontSize: displayHeight(context) * _fontSize,
+                          color: provider.getIconColor(0)))
+                ],
+              ),
             ),
-            Column(
-              children: [
-                IconButton(
-                    icon: Icon(Icons.explore_outlined,
-                        size: displayHeight(context) * _iconSize, color: _iconColor),
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(context, '/MyFilterView');
-                    }),
-                Text(" Explore", style: TextStyle(fontSize: displayHeight(context) * _fontSize)),
-              ],
+            Container(
+              width: displayWidth(context) * 0.3,
+              child: Column(
+                children: [
+                  Column(
+                    children: [
+                      IconButton(
+                          icon: Icon(Icons.explore_outlined,
+                              size: displayHeight(context) * _iconSize,
+                              color: provider.getIconColor(1)), //_iconColor),
+                          onPressed: () {
+                            Navigator.pushReplacementNamed(
+                                context, '/MyFilterView');
+                            provider.setIconColor(1);
+                          }),
+                      Text("Explore",
+                          style: TextStyle(
+                              fontSize: displayHeight(context) * _fontSize,
+                              color: provider.getIconColor(1)))
+                    ],
+                  ),
+                ],
+              ),
             ),
-            /*Column(
-              children: [
-                IconButton(
-                    icon:
-                        Icon(Icons.search, size: _iconSize, color: _iconColor),
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(context, '/MySearchView');
-                    }),
-                Text(" Search", style: TextStyle(fontSize: _fontSize)),
-              ],
-            ),*/
-            Column(
-              children: [
-                IconButton(
-                    icon: Icon(Icons.favorite_border_outlined,
-                        size: displayHeight(context) * _iconSize, color: _iconColor),
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(
-                          context, '/MyFavoritesView');
-                    }),
-                Text("    Favorites ", style: TextStyle(fontSize: displayHeight(context) * _fontSize)),
-              ],
+            Container(
+              width: displayWidth(context) * 0.3,
+              child: Column(
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.favorite_border_outlined,
+                          size: displayHeight(context) * _iconSize,
+                          color: provider.getIconColor(
+                              2)), //provider.getColor()),//_iconColor),
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(
+                            context, '/MyFavoritesView');
+                        //provider.setIconColor(2);
+                        provider.setIconColor(2);
+                      }),
+                  Text("Favorites",
+                      style: TextStyle(
+                          fontSize: displayHeight(context) * _fontSize,
+                          color: provider.getIconColor(2)))
+                ],
+              ),
             ),
           ]),
     );
